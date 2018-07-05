7521b10542d14fcb5d51ef3f8ffdeaccdcd132fa
Andres Ladino <andres.ladino@ifsttar.fr>
2018-07-05 17:19:45 +0200

    added tests

---
+++ tests/test_io.py
@@ -0,0 +1,40 @@
+"""
+    Test file for io module 
+
+    Author: Andres Ladino
+"""
+
+import os
+import sys
+import unittest
+
+print(os.getcwd())
+stestDir = os.getcwd()
+smodulePathDir = ('symupy')
+smodulePathName = os.path.join(stestDir, smodulePathDir) 
+print(smodulePathName)
+
+class Testio(unittest.TestCase):
+    """
+        Tests for the io submodule
+    """
+
+    # Submodule directory
+    stestDir = os.getcwd()
+    smodulePathDir = ('symupy')
+    smodulePathName = os.path.join(stestDir, smodulePathDir)                
+    sys.path.append(smodulePathName)
+
+    import symupy.io
+
+    def test_load_SymuViaLib(self):
+        """
+            Test library load
+        """
+        # Constructors
+        # objIo = symupy.io.SymuViaImporter()
+
+
+if __name__ == "__main__":
+    pass
+ 
