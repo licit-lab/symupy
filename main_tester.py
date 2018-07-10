c3a4a0914c43927ef0917c6b7cb1dc7145ed36fd
Andres Ladino <andres.ladino@ifsttar.fr>
2018-07-10 16:00:08 +0200

    importer multiplatform + prints load

--- main_tester.py
+++ main_tester.py
@@ -1,6 +1,9 @@
+import os 
 from simupy import SymuViaImporter
+
+file_path = []
+file_name = []
 
 x = SymuViaImporter()
 x.load_SymuViaLib()
-print(x.fullSymPath)
-print(locals())
+#print(locals())
--- simupy/io/importer.py
+++ simupy/io/importer.py
@@ -3,7 +3,40 @@
 """
 
 import os 
+import platform 
 from ctypes import cdll
+
+# Directory path where simuvia can be found
+_MACPTH = ('simupy', 'symuvia', 'Contents', 'Frameworks')
+_LINPTH = () # TODO: Assign default path in Linux
+_WINPTH = () # TODO: Assign default path in Windows
+_MACLYB = ('libSymuVia.dylib',)
+_LINLYB = () # TODO: Assign default library in Linux
+_WINLIB = () # TODO: Assign default library in Windows
+
+def get_default_lyb_path():
+    """
+        Determine the platform in use and import defa
+    """
+    os_type = platform.system()
+    dlibPathOS = {'Darwin': _MACPTH,
+                  'Linux': _LINPTH,
+                  'Windows': _WINPTH,}
+    dlibNameOS = {'Darwin': _MACLYB,
+                  'Linux': _LINLYB,
+                  'Windows': _WINLIB}
+    slibPathName = dlibPathOS[os_type]+dlibNameOS[os_type]
+    return slibPathName
+
+def build_full_path(tlibPathName):
+    """
+        :param tuple tlibPathName: Relative path for the library
+
+    """
+    scurrDir = os.getcwd()            
+    slibPathName = os.path.join(scurrDir, *tlibPathName)
+
+    return slibPathName
 
 
 class SymuViaImporter():
@@ -29,19 +62,14 @@ class SymuViaImporter():
         if self.fullSymPath:
             try:                 
                 self.olibSymuVia = cdll.LoadLibrary(self.fullSymPath)
-                print('Library successfully loaded') 
-            except: 
-                print('Library could not loaded')
+            except:                 
                 self.olibSymuVia = None
+            finally: 
+                self.print_LoadStatus()
         else:
-            scurrDir = os.getcwd()
-            # MacDefault
-            slibPathName = ('symupy',
-                            'symuvia',
-                            'Contents',
-                            'Frameworks',
-                            'libSymuVia.dylib') 
-            self.fullSymPath = os.path.join(scurrDir, *slibPathName)
+            tlibPathName = get_default_lyb_path()
+            self.fullSymPath = build_full_path(tlibPathName)
+            print(f'Defining a default path for the library at: {self.fullSymPath}')
             self.load_SymuViaLib()
 
     def load_xml_file(self, fileName):
@@ -58,3 +86,20 @@ class SymuViaImporter():
                 print('File could not be loaded')
                 self.olibSymuVia = None
             
+    def print_LoadStatus(self):
+        """
+            Status printer
+        """
+        tlibPathName = get_default_lyb_path()
+        sdefPathLib = build_full_path(tlibPathName)
+
+        try:
+            if self.olibSymuVia:
+                if sdefPathLib == self.fullSymPath:
+                    print('Default path library loaded.')    
+                else:
+                    print('External path library loaded.')
+        except AttributeError:
+            print(f'Library could not loaded. Try providing defining a new path') 
+        finally: 
+            print(f'The path: {self.fullSymPath} was used')
