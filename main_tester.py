57610e656470493452393e974b1be2c8d5dc9373
Andres Ladino <andres.ladino@ifsttar.fr>
2018-07-11 17:07:01 +0200

    Update main test

--- main_tester.py
+++ main_tester.py
@@ -1,10 +1,15 @@
 import os 
-from simupy import SymuViaImporter
+from symupy.func import Simulation 
 
 file_path = []
 file_name = []
 
-x = SymuViaImporter()
-x.load_SymuViaLib()
-print(type(x))
+fildir = '/Users/ladino/Documents/03-Code/02-Python/ISTTT2019/Network/Merge_Demand_CAV.xml'
+symdir = '/Users/ladino/Documents/03-Code/02-Python/symupy/symupy/symuvia/Contents/Frameworks/libSymuVia.dylib'
+
+x = Simulation(fildir,symdir)
+print('About to Run')
+b = x.run_Simulation()
+print('Finished with success!')
+
 #print(locals())
