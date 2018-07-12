6366a9875bdb0cda497509729cc9671f1997693c
Andres Ladino <andres.ladino@ifsttar.fr>
2018-07-12 15:50:26 +0200

    Added networking module

---
+++ symupy/func/network.py
@@ -0,0 +1,10 @@
+"""
+    This module contains information related to different 
+    networks existing in a traffic simulator 
+
+    - V2V network 
+    - Infrastructure network 
+    - V2I network 
+"""
+
+import networkx as nx
