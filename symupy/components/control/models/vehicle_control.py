704fcceb0368d6098d98a7082d8098395d4260f2
Andres Ladino <aladinoster@gmail.com>
2019-07-11 18:12:38 +0200

    :sparkles: Control of a single vehicle available

--- symupy/components/control/models/vehicle_control.py
+++ symupy/components/control/models/vehicle_control.py
@@ -1,5 +1,6 @@
-# TODO: Finish the implementation
-
+"""
+    A class to take control over vehicles 
+"""
 from dataclasses import dataclass, field
 from collections import deque
 from typing import List
@@ -7,41 +8,88 @@ from typing import List
 from symupy.components import Vehicle, VehicleList
 from symupy.utils import constants as ct
 
+import numpy as np
 
-def _dfl_lst():
-    return deque([0] * ct.BUFFER_CONTROL)
 
 @dataclass
 class VehicleControl:
     """ Controls a single vehicle"""
 
     # Local state
-    vehicle: Vehicle = field(default_factory=Vehicle)
+    mode: str = "manual"
+    time: int = ct.TIME_STEP
+    vehicle: Vehicle = Vehicle()
+    __pos: float = field(default=vehicle.totaldistance, repr=False)
+    __spd: float = field(default=vehicle.speed, repr=False)
+    __acc: float = field(default=vehicle.acceleration, repr=False)
 
-    # Internals
-    timestep: float = field(default=ct.TIME_STEP, repr=False, metadata={"unit": "s"})
-    traveldist: float = field(default=0, repr=False, metadata={"unit": "m"})
+    def compute_control(self):
+        """Compute automatic control"""
+        raise NotImplementedError
 
-    # Environment state
+    def set_manual_control(self, value, controlvar="acceleration"):
+        self.control = value
+        if controlvar == "acceleration":
+            self.acc = self.control
+        elif controlvar == "speed":
+            self.spd = self.control
+        elif controlvar == "position":
+            self.pos = self.control
+        self.update_vehicle_state()
 
-    # Historical data
-    posithistory: deque = field(default_factory=_dfl_lst, repr=False, metadata={"unit": "m"})
-    speedhistory: deque = field(default_factory=_dfl_lst, repr=False, metadata={"unit": "m/s"})
-    accelhistory: deque = field(default_factory=_dfl_lst, repr=False, metadata={"unit": "m/s^2"})
+    def update_vehicle_state(self):
+        """Update vehicle state"""
+        self.vehicle.acceleration = self.acc
+        self.vehicle.speed = self.spd
+        self.vehicle.distance = self.pos
 
-    def set_controlpolicy(self, control: float, control_type: str = "accceleration") -> None:
-        self.set_controlpolicy(control, control_type)
-        return {
-            "acceleration": self.udate_queue("speedhistory", control),
-            "speed": self.udate_queue("accelhistory", control),
-        }.get(control_type, None)
+    @property
+    def pos(self):
+        return self.__pos
 
-    def update_vehicle_state(self, vehicledata: Vehicle) -> None:
-        self.__dict__ = vehicledata.__dict__
+    @pos.setter
+    def pos(self, value):
+        new_spd = (value - self.pos) / self.time
+        self.__acc = (new_spd - self.spd) / self.time
+        self.__spd = new_spd
+        self.__pos = value
 
-    def update_deque(self, attrval: str, control: float):
-        getattr(self, attrval).append(control)
-        getattr(self, attrval).popleft()
+    @property
+    def spd(self):
+        return self.__spd
+
+    @spd.setter
+    def spd(self, value):
+        self.__acc = (value - self.spd) / self.time
+        self.__spd = value
+        self.__pos += self.time * self.spd
+
+    @property
+    def acc(self):
+        return self.__acc
+
+    @acc.setter
+    def acc(self, value):
+        self.__acc = value
+        self.__spd += self.time * self.acc
+        self.__pos += self.time * self.spd + (1 / 2) * self.acc * (self.time ** 2)
+
+    @property
+    def vehicle_state(self):
+        """ Retrive vehicle vector state"""
+        return np.array((self.vehicle.totaldistance, self.vehicle.speed, self.vehicle.acceleration))
+
+    @property
+    def control(self):
+        return self.__u
+
+    @control.setter
+    def control(self, value):
+        """ Setter for manual """
+        if self.mode == "manual":
+            self.__u = value
+            return
+        self.__u = self.compute_control()
 
 
 @dataclass
