a930c541302eff318834b6b7b8356e5b480bbdd2
Andres Ladino <aladinoster@gmail.com>
2019-07-17 17:33:00 +0200

    :alien: only control, veh state is in vehicles.py

--- symupy/components/control/models/vehicle_control.py
+++ symupy/components/control/models/vehicle_control.py
@@ -1,6 +1,7 @@
 """
     A class to take control over vehicles 
 """
+
 from dataclasses import dataclass, field
 from collections import deque
 from typing import List
@@ -11,73 +12,29 @@ from symupy.utils import constants as ct
 import numpy as np
 
 
-@dataclass
 class VehicleControl:
     """ Controls a single vehicle"""
 
-    # Local state
-    mode: str = "manual"
-    time: int = ct.TIME_STEP
-    vehicle: Vehicle = Vehicle()
-    __pos: float = field(default=vehicle.totaldistance, repr=False)
-    __spd: float = field(default=vehicle.speed, repr=False)
-    __acc: float = field(default=vehicle.acceleration, repr=False)
+    def __init__(self, vehicle: Vehicle, mode: str = "manual", time: float = ct.TIME_STEP):
+        self.mode = mode
+        self.time = time
+        self.vehicle = vehicle
+        self.__pos = vehicle.distance
+        self.__spd = vehicle.speed
+        self.__acc = vehicle.acceleration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(vehicle= {self.vehicle.__str__()},mode = {self.mode}, time= {self.time})"
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(vehicle= {self.vehicle.__str__()},mode = {self.mode}, time= {self.time})"
 
     def compute_control(self):
         """Compute automatic control"""
         raise NotImplementedError
 
     def set_manual_control(self, value, controlvar="acceleration"):
-        self.control = value
-        if controlvar == "acceleration":
-            self.acc = self.control
-        elif controlvar == "speed":
-            self.spd = self.control
-        elif controlvar == "position":
-            self.pos = self.control
-        self.update_vehicle_state()
-
-    def update_vehicle_state(self):
-        """Update vehicle state"""
-        self.vehicle.acceleration = self.acc
-        self.vehicle.speed = self.spd
-        self.vehicle.distance = self.pos
-
-    @property
-    def pos(self):
-        return self.__pos
-
-    @pos.setter
-    def pos(self, value):
-        new_spd = (value - self.pos) / self.time
-        self.__acc = (new_spd - self.spd) / self.time
-        self.__spd = new_spd
-        self.__pos = value
-
-    @property
-    def spd(self):
-        return self.__spd
-
-    @spd.setter
-    def spd(self, value):
-        self.__acc = (value - self.spd) / self.time
-        self.__spd = value
-        self.__pos += self.time * self.spd
-
-    @property
-    def acc(self):
-        return self.__acc
-
-    @acc.setter
-    def acc(self, value):
-        self.__acc = value
-        self.__spd += self.time * self.acc
-        self.__pos += self.time * self.spd + (1 / 2) * self.acc * (self.time ** 2)
-
-    @property
-    def vehicle_state(self):
-        """ Retrive vehicle vector state"""
-        return np.array((self.vehicle.totaldistance, self.vehicle.speed, self.vehicle.acceleration))
+        self.control = {"variable": controlvar, "value": value}
 
     @property
     def control(self):
