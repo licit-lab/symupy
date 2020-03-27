11753267978c1b51e1cc1a048da5d50b7a7b67e4
Andres Ladino <aladinoster@gmail.com>
2020-03-27 05:48:07 +0100

    Bump version: 0.2.2 → 0.2.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("HISTORY.md", "r") as history_file:
 
 setup(
     name="symupy",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(exclude=["tests*"]),
     install_requires=[
         "numpy>=1.16",
