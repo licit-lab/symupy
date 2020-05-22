6127d7439d1072b39acebaed09d39c6d267e38ad
Andres Ladino <aladinoster@gmail.com>
2020-05-23 00:28:42 +0200

    Bump version: 0.3.1 → 0.3.2

--- symupy/__init__.py
+++ symupy/__init__.py
@@ -1,5 +1,5 @@
 print(f"Successful import of {__name__}")
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "LICIT/SymuVia Team"
 __license__ = "MIT"
 __copyright__ = "2005 - 2020, LICIT/SymuVia Team https://www.licit.ifsttar.fr"
