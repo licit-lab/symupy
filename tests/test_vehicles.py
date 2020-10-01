af1e5bf55e1b5dae91447caff981445982c52cc2
Andres Ladino <aladinoster@gmail.com>
2020-10-01 05:04:57 +0200

    🧪 Disabling tests for redesign

--- tests/test_vehicles.py
+++ tests/test_vehicles.py
@@ -4,79 +4,79 @@ from symupy.api import Simulation, Simulator
 from symupy.components import Vehicle, VehicleList
 import platform
 
-DCT_PATH = {"Darwin": "osx-64"}
-DCT_LFN = {"Darwin": "libSymuVia.dylib"}
-_platform = platform.system()
-_libpath = DCT_PATH.get(_platform)
-_libfilen = DCT_LFN.get(_platform)
+# DCT_PATH = {"Darwin": "osx-64"}
+# DCT_LFN = {"Darwin": "libSymuVia.dylib"}
+# _platform = platform.system()
+# _libpath = DCT_PATH.get(_platform)
+# _libfilen = DCT_LFN.get(_platform)
 
 
-class TestBottleneck001(unittest.TestCase):
-    def setUp(self):
-        self.get_simulator()
-        self.get_bottleneck_001()
+# class TestBottleneck001(unittest.TestCase):
+#     def setUp(self):
+#         self.get_simulator()
+#         self.get_bottleneck_001()
 
-    def get_simulator(self):
-        self.libpath = ("lib", _libpath, _libfilen)
-        self.sim_path = os.path.join(os.getcwd(), *self.libpath)
+#     def get_simulator(self):
+#         self.libpath = ("lib", _libpath, _libfilen)
+#         self.sim_path = os.path.join(os.getcwd(), *self.libpath)
 
-    def get_bottleneck_001(self):
-        self.file_name = "bottleneck_001.xml"
-        file_path = ("tests", "mocks", "bottlenecks", self.file_name)
-        self.mocks_path = os.path.join(os.getcwd(), *file_path)
+#     def get_bottleneck_001(self):
+#         self.file_name = "bottleneck_001.xml"
+#         file_path = ("tests", "mocks", "bottlenecks", self.file_name)
+#         self.mocks_path = os.path.join(os.getcwd(), *file_path)
 
-    def test_load_bottleneck_001(self):
-        sim_case = Simulation(self.mocks_path)
-        self.assertEqual(sim_case.filename, self.mocks_path)
+#     def test_load_bottleneck_001(self):
+#         sim_case = Simulation(self.mocks_path)
+#         self.assertEqual(sim_case.filename, self.mocks_path)
 
-    # @unittest.skip("Skipping momentary")
-    def test_vehicle_in_network_bottleneck_001(self):
-        sim_instance = Simulator.from_path(self.mocks_path, self.sim_path)
-        flag = False
-        with sim_instance as s:
-            while s.do_next:
-                s.run_step()
-                if s.state.is_vehicle_in_network("0"):
-                    flag = True
+#     # @unittest.skip("Skipping momentary")
+#     def test_vehicle_in_network_bottleneck_001(self):
+#         sim_instance = Simulator.from_path(self.mocks_path, self.sim_path)
+#         flag = False
+#         with sim_instance as s:
+#             while s.do_next:
+#                 s.run_step()
+#                 if s.state.is_vehicle_in_network("0"):
+#                     flag = True
 
-        self.assertTrue(flag)
-
-
-class TestBottleneck002(unittest.TestCase):
-    def setUp(self):
-        self.get_simulator()
-        self.get_bottleneck_002()
-
-    def get_simulator(self):
-        self.libpath = ("lib", _libpath, _libfilen)
-        self.sim_path = os.path.join(os.getcwd(), *self.libpath)
-
-    def get_bottleneck_002(self):
-        self.file_name = "bottleneck_002.xml"
-        file_path = ("tests", "mocks", "bottlenecks", self.file_name)
-        self.mocks_path = os.path.join(os.getcwd(), *file_path)
-
-    def test_load_bottleneck_002(self):
-        sim_case = Simulation(self.mocks_path)
-        self.assertEqual(sim_case.filename, self.mocks_path)
-
-    def test_vehicle_in_network_bottleneck_002(self):
-        sim_instance = Simulator.from_path(self.mocks_path, self.sim_path)
-
-        flag0, flag1, flag2 = False, False, False
-        with sim_instance as s:
-            while s.do_next:
-                s.run_step()
-                if s.state.is_vehicle_in_network("0"):
-                    flag0 = True
-                if s.state.is_vehicle_in_network("1"):
-                    flag1 = True
-                if s.state.is_vehicle_in_network("2"):
-                    flag2 = True
-        self.assertTrue(flag0)
-        self.assertTrue(flag1)
-        self.assertTrue(flag2)
+#         self.assertTrue(flag)
 
 
-if __name__ == "__main__":
-    unittest.main()
+# class TestBottleneck002(unittest.TestCase):
+#     def setUp(self):
+#         self.get_simulator()
+#         self.get_bottleneck_002()
+
+#     def get_simulator(self):
+#         self.libpath = ("lib", _libpath, _libfilen)
+#         self.sim_path = os.path.join(os.getcwd(), *self.libpath)
+
+#     def get_bottleneck_002(self):
+#         self.file_name = "bottleneck_002.xml"
+#         file_path = ("tests", "mocks", "bottlenecks", self.file_name)
+#         self.mocks_path = os.path.join(os.getcwd(), *file_path)
+
+#     def test_load_bottleneck_002(self):
+#         sim_case = Simulation(self.mocks_path)
+#         self.assertEqual(sim_case.filename, self.mocks_path)
+
+#     def test_vehicle_in_network_bottleneck_002(self):
+#         sim_instance = Simulator.from_path(self.mocks_path, self.sim_path)
+
+#         flag0, flag1, flag2 = False, False, False
+#         with sim_instance as s:
+#             while s.do_next:
+#                 s.run_step()
+#                 if s.state.is_vehicle_in_network("0"):
+#                     flag0 = True
+#                 if s.state.is_vehicle_in_network("1"):
+#                     flag1 = True
+#                 if s.state.is_vehicle_in_network("2"):
+#                     flag2 = True
+#         self.assertTrue(flag0)
+#         self.assertTrue(flag1)
+#         self.assertTrue(flag2)
+
+
+# if __name__ == "__main__":
+#     unittest.main()
