74c853560f58107ab175a5e1e0897c5e434a1e92
Andres Ladino <aladinoster@gmail.com>
2021-02-23 10:10:01 +0000

    ⚙️  update on conda-build setup

--- conda/run_test.sh
+++ conda/run_test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-$PYTHON -m pytest tests/metaclass
-$PYTHON -m pytest tests/preprocess
+$PYTHON -m pytest tests/abstractions
+$PYTHON -m pytest tests/parser
 $PYTHON -m pytest tests/runtime
 $PYTHON -m pytest tests/tsc
 $PYTHON -m pytest tests/utils
