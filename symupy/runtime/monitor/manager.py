80a7bb5e627c4baa83c992a8bbce57a928bb18d9
Florian <florian.gacon@ifsttar.fr>
2021-06-09 17:30:58 +0200

    Switch to pyqtgraph

--- symupy/runtime/monitor/manager.py
+++ symupy/runtime/monitor/manager.py
@@ -1,109 +1,125 @@
-import time
+import sys
+import types
 
-import numpy as np
-import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import pyqtgraph as pg
+
+from symupy.postprocess.visunet.qtutils import Worker
 
 
-class Monitor(object):
-    def __init__(self, x_label, y_label, title, use_grid=True, style='-k', alpha=0.8):
-        self.ax = None
-
-        self.use_grid = use_grid
-        self.x_label = x_label
-        self.y_label = y_label
+class LineMonitorView():
+    def __init__(self, title, x_label, y_label, nb_plots=1, colors=['w', 'g', 'b', 'c', 'm', 'y', 'r'], line_labels=None, aggregation_period=1):
+        self.plots = [None for _ in range(nb_plots)]
         self.title = title
-        self.style = style
-        self.alpha = alpha
+        self.labels = (x_label, y_label)
 
-        self.line = None
-        self.x = []
-        self.y = []
+        self.x = [[] for _ in range(nb_plots)]
+        self.y = [[] for _ in range(nb_plots)]
 
-        self.simulator = None
+        self._plot_kwargs = {}
 
-    def set_axe(self, axe):
-        self.ax = axe
-        self.ax.set_xlabel(self.x_label)
-        self.ax.set_ylabel(self.y_label)
-        self.ax.set_title(self.title)
+        self.colors = [colors[i] for i in range(nb_plots)]
 
-        if self.use_grid:
-            self.ax.grid(True)
+        if line_labels is None:
+            self.line_labels = [None for i in range(nb_plots)]
+        else:
+            self.line_labels = line_labels
 
-    def start(self):
-        #print("Starting Monitor", self.__class__.__name__)
-        self.line = self.ax.plot([], [], self.style, alpha=self.alpha)[0]
+        self.aggregation_period = aggregation_period
 
-    def _update_graph(self, step, string):
-        #print("Update Graph", self.__class__.__name__)
-
-        x_val, y_val = self.update(step, string)
-
-        #print(x_val, y_val)
-        if (x_val, y_val) != (None, None):
-            start = time.time()
-            self.x.append(x_val)
-            self.y.append(y_val)
+    def add_plot(self, plot):
+        plot.addLegend()
+        for i in range(len(self.plots)):
+            self.plots[i] = plot.plot([], [], self._plot_kwargs, name=self.line_labels[i])
 
 
-            self.line.set_ydata(self.y)
-            self.line.set_xdata(self.x)
+    def _update_plot(self, step, string):
+        for i, p in enumerate(self.plots):
+            x_val, y_val = self.update(step, string, i)
+            if (x_val, y_val) != (None, None):
+                self.x[i].append(x_val)
+                self.y[i].append(y_val)
 
+                kwargs = {"pen":self.colors[i]}
+                kwargs.update(self._plot_kwargs)
 
-            if np.min(self.y) <= self.line.axes.get_ylim()[0] or np.max(self.y) >= self.line.axes.get_ylim()[1]:
-                self.line.axes.set_ylim([np.min(self.y) - np.std(self.y), np.max(self.y) + np.std(self.y)])
+                p.setData(self.x[i], self.y[i], **kwargs, name="test")
 
-            if np.max(self.x) >= self.line.axes.get_xlim()[1]:
-                self.line.axes.set_xlim([np.min(self.x) - 1, np.max(self.x) + 1])
-            print(f"  Time update {self.__class__.__name__}:", time.time() - start)
-
-
-
-    def end(self):
-        plt.ioff()
-
-    def update(self, step, string):
+    def update(self, step, string, ind):
         raise NotImplementedError
 
 
-class MonitorManager(object):
-    def __init__(self, figsize=(16, 9)):
-        self.fig = plt.figure(figsize=figsize)
-        self.monitors = []
+class ScatterMonitorView(LineMonitorView):
+    def __init__(self, title, x_label, y_label, symbol="+", nb_plots=1, aggregation_period=1):
+        super(ScatterMonitorView, self).__init__(title, x_label, y_label, nb_plots=nb_plots, aggregation_period=aggregation_period)
+        self._plot_kwargs = {"pen":None, "symbol":symbol}
 
-    def add_monitor(self, monitor, pos):
-        ax = self.fig.add_subplot(pos)
-        monitor.set_axe(ax)
+
+class MonitorManager():
+    def __init__(self):
+        pg.setConfigOptions(antialias=True)
+        self.monitors = []
+        self.widget = pg.GraphicsLayoutWidget(show=True, title="SymuPy Monitor")
+
+    def add_monitor(self, monitor, row, col, rowspan=1, colspan=1):
+        plot = self.widget.addPlot(row, col, rowspan, colspan, title=monitor.title, labels={'left': (monitor.labels[1]), 'bottom': (monitor.labels[0])})
+        monitor.add_plot(plot)
+
         self.monitors.append(monitor)
 
     def update(self, step, string):
-        #print("Manager update", step, string)
-        self.fig.suptitle(f"Step {step}", fontsize=16)
-        start = time.time()
-        [monitor._update_graph(step, string) for monitor in self.monitors]
-        plt.pause(1e-5)
-        print(" Total time update:", time.time() - start)
-
-    def end(self):
-        plt.show()
-
-    def start(self, simulator):
-        plt.tight_layout()
-        plt.show(block=False)
-
-        def deco_step(method):
-            def wrap(*args, **kwargs):
-                # start = time.time()
-                method(*args, **kwargs)
-                step = simulator.simulationstep
-                string = simulator.request.query
-                self.update(step, string.decode("UTF8"))
-
-                # print("Time", time.time() - start)
-            return wrap
-
-        setattr(simulator, "run_step", deco_step(simulator.run_step))
-
-        [monitor.start() for monitor in self.monitors]
+        [monitor._update_plot(step, string) for monitor in self.monitors]
 
 
+class MonitorApp(QMainWindow):
+    _app = QApplication(sys.argv)
+
+    def __init__(self, *args, **kwargs):
+        super(MonitorApp, self).__init__(*args, **kwargs)
+
+        self.central_widget = QWidget()
+        self.layout = QVBoxLayout(self.central_widget)
+        self.setCentralWidget(self.central_widget)
+
+        self.run = QPushButton("Run")
+        self.stop = QPushButton("Stop")
+        self.run.clicked.connect(self.launch_simulation)
+        self.stop.clicked.connect(self.set_stop)
+        self.layout.addWidget(self.run)
+        self.layout.addWidget(self.stop)
+
+        self.monitor_manager = MonitorManager()
+        self.layout.addWidget(self.monitor_manager.widget)
+
+        self.stop_flag = False
+
+        self.feeder = None
+
+    def add_monitor(self, monitor, row, col, rowspan=1, colspan=1):
+        self.monitor_manager.add_monitor(monitor, row, col, rowspan=rowspan, colspan=colspan)
+
+    def run_simulation(self):
+        self.stop_flag = False
+        self.process = Worker(self.launch_simulation, [])
+        self.process.start()
+
+    def set_stop(self):
+        self.stop_flag = True
+
+    def set_feeder(self, feeder):
+        assert isinstance(feeder, types.GeneratorType)
+        self.feeder = feeder
+
+    def launch_simulation(self):
+        self.stop_flag = False
+        for step, res in self.feeder:
+            if not self.stop_flag:
+                self.monitor_manager.update(step, res)
+                pg.QtGui.QApplication.processEvents()
+            else:
+                break
+
+    def launch_app(self):
+        self.show()
+        sys.exit(MonitorApp._app.exec_())
--- symupy/runtime/monitor/monitors.py
+++ symupy/runtime/monitor/monitors.py
@@ -1,36 +1,40 @@
 import re
-import time
 
 import numpy as np
 
-from symupy.runtime.monitor.manager import Monitor
+from symupy.runtime.api import Simulator
+from symupy.runtime.monitor.manager import ScatterMonitorView, LineMonitorView
+from symupy.utils.constants import DEFAULT_PATH_SYMUVIA
 
 _PATTERN_TRAJ = re.compile("<TRAJ\s(.*?)\/>")
 _PATTERN_ATTR  = re.compile('(\w+)="(.*?)"')
 _PATTERN_TIME = re.compile('val="(.*?)">')
+
 
 def _get_trajs(string):
     trajs = _PATTERN_TRAJ.findall(string)
     trajs = [{key:val for key, val in _PATTERN_ATTR.findall(t)} for t in trajs]
     return trajs
 
-class SymuviaMonitorAccumulation(Monitor):
+
+def launch_simuvia(file):
+    sim_instance = Simulator.from_path(file, DEFAULT_PATH_SYMUVIA)
+    sim_instance.trace_flow = True
+    sim_instance.step_launch_mode = "full"
+
+    with sim_instance as s:
+        while s.do_next:
+          s.run_step()
+          yield sim_instance.simulationstep, sim_instance.request.query.decode("UTF8")
+
+
+class SymuviaMonitorMFD(ScatterMonitorView):
     def __init__(self):
-        super(SymuviaMonitorAccumulation, self).__init__( 'Instant', 'VEH number', 'Accumulation')
-        self.nbveh_pattern = re.compile('<INST nbVeh="(.*?)"\s')
-
-    def update(self, step, instants):
-        nbveh = int(self.nbveh_pattern.findall(instants)[0])
-        return step, nbveh
-
-
-class SymuviaMonitorMFD(Monitor):
-    def __init__(self):
-        super(SymuviaMonitorMFD, self).__init__('Accumulation', '<v> x accumulation', 'MFD', style='k+')
+        super(SymuviaMonitorMFD, self).__init__('MFD', 'Accumulation', 'production')
         self.nbveh_pattern = re.compile('<INST nbVeh="(.*?)"\s')
         self.vit_pattern = re.compile('vit="(.*?)"')
 
-    def update(self, step, instants):
+    def update(self, step, instants, ind):
         nbveh = int(self.nbveh_pattern.findall(instants)[0])
         speeds = [float(v) for v in self.vit_pattern.findall(instants)]
         if speeds:
@@ -40,19 +44,31 @@ class SymuviaMonitorMFD(Monitor):
             return None, None
 
 
-class SymuviaMonitorVEH(Monitor):
-    def __init__(self, id, indicator):
+class SymuviaMonitorAccumulation(LineMonitorView):
+    def __init__(self):
+        super(SymuviaMonitorAccumulation, self).__init__('Accumulation', 'Instant', 'VEH number')
+        self.nbveh_pattern = re.compile('<INST nbVeh="(.*?)"\s')
+
+    def update(self, step, instants, ind):
+        nbveh = int(self.nbveh_pattern.findall(instants)[0])
+        return step, nbveh
+
+
+class SymuviaMonitorVEH(LineMonitorView):
+    def __init__(self, ids, indicator):
         self._indicators = {"speed": self._update_speed,
                             "acceleration": self._update_acceleration,
                             "distance": self._update_distance}
         assert indicator in self._indicators
-        super(SymuviaMonitorVEH, self).__init__('Instant', indicator, f'VEH{id} {indicator}')
-        self.id = id
+        super(SymuviaMonitorVEH, self).__init__(f'VEH{ids} {indicator}', 'Instant', indicator, nb_plots=len(ids))
+        self.ids = ids
 
         self.indicator = indicator
 
         self._dst = 0
         self._lastpos = None
+
+        self.line_labels = [str(id) for id in ids]
 
 
     def _update_speed(self, traj):
@@ -71,12 +87,12 @@ class SymuviaMonitorVEH(Monitor):
             self._lastpos = curr_pos
         return self._dst
 
-    def update(self, step, instants):
+    def update(self, step, instants, ind):
         trajs = _get_trajs(instants)
         veh_state = None
         #time = float(_PATTERN_TIME.findall(instants)[0])
         for t in trajs:
-            if int(t["id"]) == self.id:
+            if int(t["id"]) == self.ids[ind]:
                 veh_state = t
                 break
 
@@ -87,33 +103,116 @@ class SymuviaMonitorVEH(Monitor):
             return None, None
 
 
+class SymuviaMonitorTTT(LineMonitorView):
+    def __init__(self, zone, aggregation_period=1):
+        super(SymuviaMonitorTTT, self).__init__('Total Travel Time', 'Instant', 'Time')
+        self.zone = set(zone)
+        self.aggregation_period = aggregation_period
+
+        self.vehs= {}
+        self.ttt = 0
+
+    def update(self, step, instants, ind):
+        if step%self.aggregation_period==0:
+            time = float(_PATTERN_TIME.findall(instants)[0])
+            trajs = _get_trajs(instants)
+            curr_t = set()
+            for t in trajs:
+                if t["tron"] in self.zone and t["id"] in self.vehs:
+                    self.ttt += time - self.vehs[t["id"]]
+                    self.vehs[t["id"]] = time
+                if t["tron"] in self.zone:
+                    curr_t.add(t["id"])
+                    self.vehs[t["id"]] = time
+
+            [self.vehs.pop(t) for t in list(self.vehs.keys()) if t not in curr_t]
+
+            return step, self.ttt
+        else:
+            return None, None
+
+
+class SymuviaMonitorTTD(LineMonitorView):
+    def __init__(self, zone, aggregation_period=1):
+        super(SymuviaMonitorTTD, self).__init__('Total Travel Distance', 'Instant', 'Distance')
+        self.zone = set(zone)
+        self.aggregation_period = aggregation_period
+
+        self.vehs= {}
+        self.ttd = 0
+
+    def update(self, step, instants, ind):
+        if step%self.aggregation_period==0:
+            trajs = _get_trajs(instants)
+            curr_t = set()
+            for t in trajs:
+                pos = np.array([float(t["abs"]), float(t["ord"])])
+                if t["tron"] in self.zone and t["id"] in self.vehs:
+                    self.ttd += np.linalg.norm(pos - self.vehs[t["id"]])
+                    self.vehs[t["id"]] = pos
+                if t["tron"] in self.zone:
+                    curr_t.add(t["id"])
+                    self.vehs[t["id"]] = pos
+
+            [self.vehs.pop(t) for t in list(self.vehs.keys()) if t not in curr_t]
+
+            return step, self.ttd
+        else:
+            return None, None
+
+
+class SymuviaMonitorFlux(LineMonitorView):
+    def __init__(self, zone, aggregation_period=1):
+        super(SymuviaMonitorFlux, self).__init__('Flux', 'Instant', 'Number', nb_plots=2, line_labels=["InFlux", "OutFlux"])
+        self.zone = set(zone)
+        self.aggregation_period = aggregation_period
+
+        self.influx = 0
+        self.outflux = 0
+
+        self.invehs = set()
+
+    def update(self, step, instants, ind):
+        trajs = _get_trajs(instants)
+
+        if ind == 0:
+            for t in trajs:
+                if t["id"] not in self.invehs and t["tron"] in self.zone:
+                    self.influx += 1
+                    self.invehs.add(t["id"])
+            val = self.influx
+            print("in", val)
+            if step % self.aggregation_period == 0:
+                self.influx= 0
+        else:
+            curr_vehs = [t["id"] for t in trajs]
+            to_del = set()
+            for t in self.invehs:
+                if t not in curr_vehs:
+                    self.outflux += 1
+                    to_del.add(t)
+            print(to_del, self.outflux)
+            self.invehs = self.invehs.difference(to_del)
+            val = self.outflux
+            print("out",val)
+            if step % self.aggregation_period == 0:
+                self.outflux= 0
+
+        if step % self.aggregation_period == 0:
+            return step, val
+        else:
+            return None, None
+
 
 if __name__ == "__main__":
-    from symupy.runtime.api import Simulator
-    from symupy.utils.constants import DEFAULT_PATH_SYMUVIA
-    from symupy.runtime.monitor import MonitorManager
+    from symupy.runtime.monitor.manager import MonitorApp
 
-    sim_instance = Simulator.from_path("/Users/florian.gacon/Work/data/SingleLink_symuvia_withcapacityrestriction.xml", DEFAULT_PATH_SYMUVIA)
-    sim_instance.trace_flow = True
-    sim_instance.step_launch_mode = "full"
-
-    mm1 = MonitorManager()
-    #mm1.add_monitor(SymuviaMonitorVEH(29, "distance"), 222)
-    #mm1.add_monitor(SymuviaMonitorVEH(29, "speed"), 221)
-    mm1.add_monitor(SymuviaMonitorMFD(), 121)
-    mm1.add_monitor(SymuviaMonitorAccumulation(), 122)
-
-    mm1.start(sim_instance)
-
-
-    with sim_instance as s:
-        #while s.do_next:
-         #   s.run_step()
-
-        for _ in range(400):
-            start = time.time()
-            s.run_step()
-            print("Total time step:", time.time() - start)
-
-    mm1.end()
-
+    manager = MonitorApp()
+    manager.add_monitor(SymuviaMonitorMFD(), 0, 0)
+    manager.add_monitor(SymuviaMonitorAccumulation(), 0, 1)
+    manager.add_monitor(SymuviaMonitorVEH([3,58], "speed"), 1, 0)
+    manager.add_monitor(SymuviaMonitorTTT(["L_0"], aggregation_period=1), 1, 1)
+    manager.add_monitor(SymuviaMonitorTTD(["L_0"], aggregation_period=1), 0, 2, rowspan=2)
+    manager.add_monitor(SymuviaMonitorFlux(["L_0"], aggregation_period=10), 2, 0, colspan=3)
+    manager.set_feeder(launch_simuvia("/Users/florian/Work/visunet/data/SingleLink_symuvia_withcapacityrestriction.xml"))
+    manager.launch_app()
